3e813c120aa003fd32220319e1c634d0bb234419
John Chong <johnchong.99.17@gmail.com>
2023-04-18 18:41:02 -0500

    refactored for all OS

--- wordfinder.py
+++ wordfinder.py
@@ -4,6 +4,7 @@ from random import randint
 class WordFinder:
     """
     Word Finder: finds random words from a dictionary.
+
     """
 
     def __init__(self, path):
@@ -12,13 +13,17 @@ class WordFinder:
 
         >>> words = WordFinder(path='normal_words.txt')
         5 words read
-        """
 
+        """
         self.path = path
         self.words = self.read_file_and_append()
         self.print_words_read()
 
     def __repr__(self):
+        """
+        Generate summary for RandomWordFinder instance
+
+        """
         return f"path={self.path}, words={self.words}"
 
     def read_file_and_append(self):
@@ -30,16 +35,17 @@ class WordFinder:
 
         >>> words.words == ['one', 'two', 'three', 'four', 'five']
         True
+
         """
 
         file = open(self.path, "r")
-        file_text = file.read()
-        split_words = file_text.split("\n")
-        return split_words
+        file_text = file.readlines()
+        return [split.strip() for split in file_text]
 
     def print_words_read(self):
         """
         prints number of words in the read file
+
         """
         print(f"{len(self.words)} words read")
 
@@ -54,13 +60,16 @@ class WordFinder:
         True
 
         """
-        rand_index = randint(0, len(self.words))
+        rand_index = randint(0, len(self.words)-1)
         return self.words[rand_index]
+
+
 
 
 class RandomWordFinder(WordFinder):
     """
     Word finder that ignores blank lines and lines that start with #
+
     """
 
     def __init__(self, path):
@@ -69,8 +78,16 @@ class RandomWordFinder(WordFinder):
 
         >>> words = RandomWordFinder(path='words.txt')
         5 words read
+
         """
         super().__init__(path)
+
+    def __repr__(self):
+        """
+        Generate summary for RandomWordFinder instance
+
+        """
+        super().__repr__()
 
     def read_file_and_append(self):
         """
@@ -82,14 +99,8 @@ class RandomWordFinder(WordFinder):
 
         >>> words.words == ['one', 'two', 'three', 'four', 'five']
         True
+
         """
-        file = open(self.path, "r")
-        file_text = file.read()
+        basic_filtered = super().read_file_and_append()
 
-        # replace all instances of \n with space
-        # make list of words
-        # remove words that start with #
-
-        filtered_words = file_text.replace("\n", " ").split(" ")
-
-        return [word for word in filtered_words if word != "" and word[0] != "#"]
+        return [word for word in basic_filtered if word != "" and word[0] != "#"]

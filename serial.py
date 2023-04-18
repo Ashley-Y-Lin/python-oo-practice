4f1eb6315301a4bbf9371cccd35430f2e42148fc
Ashley Lin <ashleyylin11@gmail.com>
2023-04-18 15:24:17 -0700

    add serial.py and started on wordfinder

--- serial.py
+++ serial.py
@@ -1,19 +1,53 @@
 class SerialGenerator:
-    """Machine to create unique incrementing serial numbers.
-    
-    >>> serial = SerialGenerator(start=100)
+    """Machine to create unique incrementing serial numbers."""
 
-    >>> serial.generate()
-    100
+    def __init__(self, start):
+        """
+        Creates a new serial number
+        >>> serial = SerialGenerator(start=100)
+        """
+        self.start = start
+        self.count = start
 
-    >>> serial.generate()
-    101
+    def __repr__(self):
+        return f"<start value={self.start}, current value={self.count}>"
 
-    >>> serial.generate()
-    102
+    def generate(self):
+        """
+        Returns the next sequential number
+        >>> serial = SerialGenerator(start=100)
 
-    >>> serial.reset()
+        >>> serial.generate()
+        100
 
-    >>> serial.generate()
-    100
-    """
+        >>> serial.generate()
+        101
+
+        >>> serial.generate()
+        102
+        """
+        # could also just subtract 1 from return value
+        output = self.count
+        self.count += 1
+        return output
+
+    def reset(self):
+        """
+        Resets the serial number to original start number
+        >>> serial = SerialGenerator(start=100)
+
+        >>> serial.generate()
+        100
+
+        >>> serial.generate()
+        101
+
+        >>> serial.generate()
+        102
+
+        >>> serial.reset()
+
+        >>> serial.generate()
+        100
+        """
+        self.count = self.start
--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,19 @@
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, path):
+        self.path = path
+        self.words = []
+        self.read_file_and_append()
+        self.print_words_read()
+
+    def read_file_and_append:
+        #reads file and appends words to self.words
+        file = open(self.path, "r")
+        file_text = file.read()
+
+    def print_words_read:
+        print(f"{len(self.words)} words read")
+
+    def random:
+        #random word from self.words
